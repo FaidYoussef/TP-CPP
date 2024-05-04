bdf461a4ea9455ab1a8907f27a71275ca6767b51
FaidYoussef <youssef.faid@grenoble-inp.org>
2024-05-04 20:04:19 +0200

    Supprimer le dossier build/ du suivi de Git

--- build/src/CMakeFiles/Cellule.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for Cellule.
-# This may be replaced when dependencies are built.
--- build/test/CMakeFiles/gtest.dir/gtest.cxx.o.d
+++
@@ -1,2 +0,0 @@
-test/CMakeFiles/gtest.dir/gtest.cxx.o: \
- /home/youssef/2A/CPP/Lab4/test/gtest.cxx /usr/include/stdc-predef.h
