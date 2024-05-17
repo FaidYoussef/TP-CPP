a89e98b1293e9b3c75d136d69dd5e42ec7438979
FaidYoussef <youssef.faid@grenoble-inp.org>
2024-05-17 09:46:08 +0200

    exempleUnivers is now a demo file

---
+++ demo/exempleUnivers.cxx
@@ -0,0 +1,31 @@
+
+
+// tester la classe univers
+
+#include <iostream>
+
+#include "Univers.hxx"
+#include "Cellule.hxx"
+#include "Particule3D.hxx"
+#include "Vector3D.hxx"
+#include <cmath>
+#include <chrono> // Pour mesurer le temps
+#include <vector>
+#include <list>
+
+
+int main() {
+    std::cout << "Evolution en cours" << std::endl;
+
+    Univers univers = Univers(3, 250, 45, 0, 2.5, 0.05, 1.95);
+
+    univers.initialiser();
+
+
+    std::cout << "Evolution" << std::endl;
+    univers.evolution();
+
+    std::cout << "FIN" << std::endl;
+    return 0;
+
+}
